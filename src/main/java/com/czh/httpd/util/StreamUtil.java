f498decd561e3d9b2d05741dfc0d85985ce2c8ee
czh <czhwin@outlook.com>
2020-12-03 11:20:14 +0800

    去掉多余代码

--- src/main/java/com/czh/httpd/util/StreamUtil.java
+++ src/main/java/com/czh/httpd/util/StreamUtil.java
@@ -23,24 +23,4 @@ public class StreamUtil {
         }
         return sb.toString();
     }
-
-    public static String getHttpRequestHeader(InputStream inputStream, String charsetName) throws IOException {
-        if (charsetName == null || "".equals(charsetName)) {
-            charsetName = "UTF-8";
-        }
-        String line;
-        StringBuilder sb = new StringBuilder();
-        sb.append("/r/n");
-        BufferedReader read = new BufferedReader(new InputStreamReader(inputStream,charsetName));
-        String lastLine = "";
-        while ((line = read.readLine()) != null) {
-            // 注意指定编码格式，发送方和接收方一定要统一，建议使用UTF-8
-            sb.append(line).append("/r/n");
-            if (StringUtils.isBlank(line)) {
-                break;
-            }
-            lastLine = line;
-        }
-        return sb.toString();
-    }
 }
